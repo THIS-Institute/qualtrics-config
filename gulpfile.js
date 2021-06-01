95821b13fc75312aa62a24e959bb5b790e7902ac
André Sartori <afs25@cam.ac.uk>
2021-06-01 10:47:29 +0100

    Added bundling of css files

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,32 @@
 'use strict';
 
 var gulp = require('gulp');
+var concat = require('gulp-concat')
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 
-gulp.task('default', function () {
-    return gulp.src('./scss/*.scss')
+gulp.task('compile-sass', function () {
+    return gulp.src('scss/main.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(autoprefixer({
             overrideBrowserslist: ['last 10 versions'],
             cascade: false
         }))
-        .pipe(gulp.dest('./css'));
+        .pipe(gulp.dest('css'));
 });
+
+gulp.task('pack-css', function () {
+    return gulp.src([
+        'jquery-ui-1.12.1.custom/jquery-ui.css',
+        'jquery-ui-1.12.1.custom/jquery-ui.structure.css',
+        'jquery-ui-1.12.1.custom/jquery-ui.theme.css',
+        'css/main.css',
+    ])
+        .pipe(concat('bundle.css'))
+        .pipe(gulp.dest('public/build/css'));
+});
+
+gulp.task('default', gulp.series('compile-sass', 'pack-css'));
 
 gulp.task('watch', function () {
     gulp.watch('./scss/**/*.scss', gulp.series('default'));
